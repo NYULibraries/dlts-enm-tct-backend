2036f9d85a5897db816f4cddb2b71dde9c0b2344
Matt Broach <go.for.dover@gmail.com>
2017-03-07 22:31:40 -0500

    cleaned scripts folder

--- scripts/automatic_relations.py
+++
@@ -1,7 +0,0 @@
-from otcore.relation.processing import global_containment
-from manuscripts.processing import nyu_global_multiple_tokens
-
-def run():
-    global_containment()
-    nyu_global_multiple_tokens()
-
